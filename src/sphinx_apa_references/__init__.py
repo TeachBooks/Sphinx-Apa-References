7a0e23ffa8b1759d8c4087be6c7ac722082c0048
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-11-16 13:44:09 +0100

    Set default BibTeX style to APA in Sphinx setup
    Adds 'bibtex_default_style' config value with 'apa' as the default in the Sphinx extension setup function to ensure APA formatting is used by default.

--- src/sphinx_apa_references/__init__.py
+++ src/sphinx_apa_references/__init__.py
@@ -29,6 +29,7 @@ class MyReferenceStyle(AuthorYearReferenceStyle):
     bracket_year: BracketStyle = field(default_factory=bracket_style)
 
 def setup(app):
+    app.add_config_value("bibtex_default_style", "apa", "env")
     register_plugin('pybtex.style.labels', 'myapa', MyAPALabelStyle)
     register_plugin('pybtex.style.formatting', 'myapastyle', MyAPAStyle)
     sphinxcontrib.bibtex.plugin.register_plugin('sphinxcontrib.bibtex.style.referencing','author_year_round', MyReferenceStyle)
