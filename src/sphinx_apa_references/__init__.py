d0a9b32e1da61eddb0b65065fc38f6101069b393
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-11-16 14:27:59 +0100

    Add APA-enforcing bibliography directive
    Introduces APABibliographyDirective to force 'style=apa' for bibliography directives unless overridden. Registers the new directive in Sphinx setup and ensures sphinxcontrib.bibtex extension is initialized.

--- src/sphinx_apa_references/__init__.py
+++ src/sphinx_apa_references/__init__.py
@@ -6,6 +6,18 @@ from .pybtexapastyle.labels.apa import LabelStyle as APALabelStyle
 from pybtex.plugin import register_plugin
 from sphinxcontrib.bibtex.style.referencing import BracketStyle
 from sphinxcontrib.bibtex.style.referencing.author_year import AuthorYearReferenceStyle
+from sphinxcontrib.bibtex.directives import BibliographyDirective
+
+class APABibliographyDirective(BibliographyDirective):
+    """Same as BibliographyDirective, but forces style='apa'."""
+
+    def run(self):
+        # ensure 'style' option is set to 'apa' unless user overrides it
+        self.options.setdefault("style", "apa")
+        nodes = super().run()
+        print(nodes[0].children)
+        return nodes
+
 
 class MyAPALabelStyle(APALabelStyle):
     def format_label(self, entry):
@@ -29,6 +41,8 @@ class MyReferenceStyle(AuthorYearReferenceStyle):
     bracket_year: BracketStyle = field(default_factory=bracket_style)
 
 def setup(app):
+    app.setup_extension("sphinxcontrib.bibtex")
     register_plugin('pybtex.style.labels', 'myapa', MyAPALabelStyle)
     register_plugin('pybtex.style.formatting', 'myapastyle', MyAPAStyle)
     sphinxcontrib.bibtex.plugin.register_plugin('sphinxcontrib.bibtex.style.referencing','author_year_round', MyReferenceStyle)
+    app.add_directive('bibliography', APABibliographyDirective, override=True)
